66f0eb68e801ee130146e5e92f479760babff775
William Abboud <william_y_abboud@abv.bg>
2018-04-20 18:03:09 +0300

    Separated Data, DataContext, and DataModels in their own projects

---
+++ Architecture.Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Architecture.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Architecture.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ Architecture/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Architecture.Startup))]
+namespace Architecture
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
